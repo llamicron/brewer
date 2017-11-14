721045662287fee9e414b39506b06c487f007c8d
Luke Sweeney <luke@thesweeneys.org>
2017-11-14 16:40:11 -0600

    BrewerBot will now look for env variables first

--- tests/test_slack.py
+++ tests/test_slack.py
@@ -6,10 +6,15 @@ from ..brewer.slack import BrewerBot
 class SlackTestCase(unittest.TestCase):
     def setUp(self):
         self.bot = BrewerBot()
-        self.bot.channel = "@luke"
+        self.bot.channel = '@luke'
 
-    def test_get_token_from_file(self):
+    def test_try_get_token_from_file(self):
         assert isinstance(self.bot.get_token_from_file(), str)
 
     def test_send_message(self):
         assert self.bot.send(" :beer: Test's are running")
+
+    def test_default_channel(self):
+        os.environ['brewer_channel'] = 'a_new_channel'
+        self.bot = BrewerBot()
+        assert self.bot.channel == 'a_new_channel'
