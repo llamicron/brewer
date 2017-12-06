3bccd63c0272924a93e3984adc5419d08aaa351e
Luke Sweeney <luke@thesweeneys.org>
2017-12-05 23:29:04 -0600

    A few small updates

--- setup.py
+++ setup.py
@@ -11,5 +11,8 @@ setup(
     url='https://github.com/llamicron/brewer',
     download_url="https://github.com/llamicron/brewer/archive/%s.tar.gz" % VERSION,
     keywords=['beer', 'python', 'brewing'],
+    scripts=[
+        'brewer/__main__.py'
+    ],
     classifiers=[],
 )
